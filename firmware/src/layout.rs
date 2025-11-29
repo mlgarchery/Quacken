5104eee2b4b4ab31be57ade4c84b40bc23d2a318
mlgarchery <martial.garchery@gmail.com>
2025-11-29 02:03:15 +0100

    ctrl renaming

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -12,14 +12,14 @@ pub type QuackenLayout = layout::Layout<COLS, ROWS, LAYER_COUNT, ()>;
 // common shortcuts -- adapt to your OS layout if necessary, e.g. for Ergol:
 // const CLOSE: Action<()> = m(&[KeyCode::LCtrl, KeyCode::T].as_slice());
 // const COPY:  Action<()> = m(&[KeyCode::LCtrl, KeyCode::W].as_slice());
-const CMD: KeyCode = KeyCode::LCtrl; // LGui for macOS
-const UNDO: Action<()> = m(&[CMD, KeyCode::Z].as_slice());
-const CUT: Action<()> = m(&[CMD, KeyCode::X].as_slice());
-const COPY: Action<()> = m(&[CMD, KeyCode::C].as_slice());
-const PASTE: Action<()> = m(&[CMD, KeyCode::V].as_slice());
-const ALL: Action<()> = m(&[CMD, KeyCode::A].as_slice());
-const SAVE: Action<()> = m(&[CMD, KeyCode::S].as_slice());
-const CLOSE: Action<()> = m(&[CMD, KeyCode::W].as_slice());
+const CTRL: KeyCode = KeyCode::LCtrl; // would give CMD on macOS
+const UNDO: Action<()> = m(&[CTRL, KeyCode::Z].as_slice());
+const CUT: Action<()> = m(&[CTRL, KeyCode::X].as_slice());
+const COPY: Action<()> = m(&[CTRL, KeyCode::C].as_slice());
+const PASTE: Action<()> = m(&[CTRL, KeyCode::V].as_slice());
+const ALL: Action<()> = m(&[CTRL, KeyCode::A].as_slice());
+const SAVE: Action<()> = m(&[CTRL, KeyCode::S].as_slice());
+const CLOSE: Action<()> = m(&[CTRL, KeyCode::W].as_slice());
 
 // other shortcuts
 const STB: Action<()> = m(&[KeyCode::RShift, KeyCode::Tab].as_slice());
@@ -35,48 +35,17 @@ pub static LAYERS: layout::Layers<COLS, ROWS, LAYER_COUNT, ()> = layout::layout!
         [ LShift Z    X    C    V    B         N    M    ,    .    /    RShift ],
         [ n n n         LCtrl Space (1)        LGui Space RAlt             n n n ],
     }
-
-    // Navigation, Numbers, Cmd-ZXCV:
-    //  - either pick the `NumNav` layer -- simple, gets the job done
-    //  - or the `NumRow` + `VimNav` layers -- less intuitive but sharper
-
     { // NumNav
         [ t        Tab  Home  Up   End  PgUp      n    7    8    9    n    Delete ],
         [ t    CapsLock Left Down Right PgDown    n    4    5    6    0         t ],
         [ t    {UNDO}{CUT}{COPY}{PASTE}{STB}      n    1    2    3    n         t ],
         [ n n n               t  Space   t       (2)  LAlt  t               n n n ],
     }
-    // { // NumRow
-    //     [ t         !    @    #    $    %         ^    &    *   '('  ')'        t ],
-    //     [ t         1    2    3    4    5         6    7    8    9    0         t ],
-    //     [ t    {UNDO}{CUT}{COPY}{PASTE} n         n    n    n    n    n         t ],
-    //     [ n n n               t    t    t         t    t    t               n n n ],
-    // }
-    // { // VimNav
-    //     [ t CapsLock {CLOSE}{BCK}{FWD}  n      Home PgDown PgUp  End  n    Delete ],
-    //     [ t     {ALL}{SAVE}{STB}  Tab   n      Left  Down   Up  Right n         t ],
-    //     [ t    {UNDO}{CUT}{COPY}{PASTE} n         n    n    n    n    n         t ],
-    //     [ n n n               t    t    t         t    t    t               n n n ],
-    // }
-
     // Function Keys
-
     {
         [ t        F1   F2   F3   F4    n         n Pause PScreen    t     n    t  ],
         [ t        F5   F6   F7   F8    n         n  {ALL}  {BCK}  {FWD} {SAVE} t  ],
         [ t        F9   F10  F11  F12   n         n    n    n    n      {CLOSE} t  ],
         [ n n n               t    t    t         t    t    t               n n n  ],
     }
-
-    // Symbols: use this rather than `RAlt` if your keyboard layout has no AltGr key
-    // (or if you prefer sending the proper key code, e.g. for keyboard shortcuts).
-    // On a non-QWERTY layout, you'll have to make a few adjustments.
-
-    // { // QWERTY
-    //     [ t         ^    <    >    $    %         @    &    *  '\''  '`'        t ],
-    //     [ t        '{'  '('  ')'  '}'   =       '\\'   +    -    /   '"'        t ],
-    //     [ t         ~   '['  ']'  '_'   #         |    !    ;    :    ?         t ],
-    //     [ n n n              (2) Space  t         t    t    t               n n n ],
-    // }
-
 };
