1a6eff00b488b98bae05441bb75851c0b1c1d27b
mlgarchery <martial.garchery@gmail.com>
2025-12-28 22:57:24 +0100

    adding left alt

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -13,13 +13,16 @@ pub type QuackenLayout = layout::Layout<COLS, ROWS, LAYER_COUNT, ()>;
 // const CLOSE: Action<()> = m(&[KeyCode::LCtrl, KeyCode::T].as_slice());
 // const COPY:  Action<()> = m(&[KeyCode::LCtrl, KeyCode::W].as_slice());
 const CTRL: KeyCode = KeyCode::LCtrl; // would give CMD on macOS
-const UNDO: Action<()> = m(&[CTRL, KeyCode::Z].as_slice());
-const CUT: Action<()> = m(&[CTRL, KeyCode::X].as_slice());
+const SUPER: KeyCode = KeyCode::LGui; // would give CMD on macOS
+// const UNDO: Action<()> = m(&[CTRL, KeyCode::Z].as_slice());
+// const CUT: Action<()> = m(&[CTRL, KeyCode::X].as_slice());
 const COPY: Action<()> = m(&[CTRL, KeyCode::C].as_slice());
 const PASTE: Action<()> = m(&[CTRL, KeyCode::V].as_slice());
 const ALL: Action<()> = m(&[CTRL, KeyCode::A].as_slice());
 const SAVE: Action<()> = m(&[CTRL, KeyCode::S].as_slice());
 const CLOSE: Action<()> = m(&[CTRL, KeyCode::W].as_slice());
+const FIRST: Action<()> = m(&[SUPER, KeyCode::Kb1].as_slice()); // first windows focus 
+const SECOND: Action<()> = m(&[SUPER, KeyCode::Kb2].as_slice()); // second windows focus   s
 
 // other shortcuts
 const STB: Action<()> = m(&[KeyCode::RShift, KeyCode::Tab].as_slice());
@@ -30,16 +33,16 @@ const FWD: Action<()> = Action::KeyCode(KeyCode::MediaForward);
 pub static LAYERS: layout::Layers<COLS, ROWS, LAYER_COUNT, ()> = layout::layout! {
 
     { // base layer -- don't worry about lhe key names, this will reflect your OS keyboard layout
-        [ Escape Q    W    E    R    T         Y    U    I    O    P    BSpace ],
-        [ Tab    A    S    D    F    G         H    J    K    L    ;     Enter ],
-        [ LShift Z    X    C    V    B         N    M    ,    .    /    RShift ],
-        [ n n n         LCtrl Space (1)        LGui Space RAlt             n n n ],
+        [ Escape  Q    W    E    R    T           Y    U    I    O    P    BSpace ],
+        [ LAlt    A    S    D    F    G           H    J    K    L    ;     Enter ],
+        [ LShift  Z    X    C    V    B           N    M    ,    .    /    RShift ],
+        [ n n n          LCtrl Space (1)          LGui Space RAlt           n n n ],
     }
     { // NumNav
-        [ t        Tab  Home  Up   End  PgUp      n    7    8    9    n    Delete ],
-        [ t    CapsLock Left Down Right PgDown    n    4    5    6    0         t ],
-        [ t    {UNDO}{CUT}{COPY}{PASTE}{STB}      n    1    2    3    n         t ],
-        [ n n n               t  Space   t       (2)  LAlt  t               n n n ],
+        [ t        t        Tab  Up   End  PgUp      n    7    8    9    n    Delete ],
+        [ LAlt    CapsLock Left Down Right PgDown    n    4    5    6    0         t ],
+        [ LShift  {FIRST}{SECOND}{COPY}{PASTE}{STB}  n    1    2    3    n    RShift ],
+        [ n n n            LCtrl  Space     t        (2)  LAlt  t              n n n ],
     }
     // Function Keys
     {
